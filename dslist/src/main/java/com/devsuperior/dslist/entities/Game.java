482504b2b733f0db8271ffba1e159aa0ab5fd24a
vitorSchlickmann <vitorsluciano@gmail.com>
2025-01-21 00:22:38 -0300

    seed games

--- dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,6 +1,11 @@
 package com.devsuperior.dslist.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
 
 import java.util.Objects;
 
@@ -16,21 +21,27 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imageUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imageUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imageUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imageUrl = imageUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -77,11 +88,19 @@ public class Game {
     }
 
     public String getPlatform() {
-        return platform;
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatform(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getLongDescription() {
@@ -104,11 +123,11 @@ public class Game {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Game game = (Game) o;
-        return Objects.equals(id, game.id) && Objects.equals(title, game.title) && Objects.equals(year, game.year) && Objects.equals(genre, game.genre) && Objects.equals(platform, game.platform) && Objects.equals(imageUrl, game.imageUrl) && Objects.equals(shortDescription, game.shortDescription) && Objects.equals(longDescription, game.longDescription);
+        return Objects.equals(id, game.id) && Objects.equals(title, game.title) && Objects.equals(year, game.year) && Objects.equals(genre, game.genre) && Objects.equals(platforms, game.platforms) && Objects.equals(score, game.score) && Objects.equals(imageUrl, game.imageUrl) && Objects.equals(shortDescription, game.shortDescription) && Objects.equals(longDescription, game.longDescription);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, year, genre, platform, imageUrl, shortDescription, longDescription);
+        return Objects.hash(id, title, year, genre, platforms, score, imageUrl, shortDescription, longDescription);
     }
 }
